02e4e46ffb23222f8138699bdc831a274f563e68
samiti3109-source <samiti3109@gmail.com>
2026-07-27 00:04:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -292,6 +292,15 @@ def edit_profile():
     return render_template("edit_profile.html")
 
 
+import os
+
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
